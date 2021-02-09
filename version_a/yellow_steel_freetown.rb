5ce0f6dd5b9fcd71832f595db078acb8efcd5ec6
Jason Bornhoft <jmvbxx@gmail.com>
2021-02-08 21:44:08 -0500

    Some clean up and learning

--- version_a/yellow_steel_freetown.rb
+++ version_a/yellow_steel_freetown.rb
@@ -1,5 +1,5 @@
 TRAVEL_DISTANCE = 160       # kilometers
-PAYLOAD_CAPACITY = 50_000   # kilgrams including the rocket itself
+PAYLOAD_CAPACITY = 50_000   # kilograms including the rocket itself
 FUEL_CAPACITY = 1_514_100   # liters of fuel, already included in the payload total
 BURN_RATE = 168_233         # liters per minute
 AVERAGE_SPEED = 1_500       # kilometers/hr
@@ -29,19 +29,20 @@ loop do
   afterburner = gets.chomp
   break unless afterburner.downcase.start_with?('y')
   puts 'Afterburner engaged!'
-  break if random_seed % 3 == 0
+  if random_seed % 3 == 0
+    puts 'Mission aborted!'
+    break
+  end
 
   print 'Release support structures? (Y/n) '
   release = gets.chomp
   break unless release.downcase.start_with?('y')
   puts 'Support structures released!'
-  # break if random_seed % 3 == 0
 
   print 'Perform cross-checks? (Y/n) '
   cross_checks = gets.chomp
   break unless cross_checks.downcase.start_with?('y')
   puts 'Cross-checks performed!'
-  # break if random_seed % 3 == 0
 
   print 'Launch? (Y/n) '
   launch = gets.chomp
@@ -53,11 +54,10 @@ loop do
     break
   end
 
+  mission_time = 0
+  total_distance_traveled = 0
+  count = 0
   loop do
-
-    mission_time = 0
-    total_distance_traveled = 0
-
     while total_distance_traveled <= TRAVEL_DISTANCE
       mission_time += 30
       current_speed = rand(1400..1600).to_f
@@ -72,15 +72,13 @@ loop do
       puts "  Current distance traveled: #{total_distance_traveled.round(2)} km"
       puts "  Time to destination: #{time_to_destination.round(2)} seconds"
     end
-
+    print 'Do you wish to continue? (Y/n) '
+    continue = gets.chomp
+    count += 1
+    unless continue.downcase.start_with?('y')
+      puts "Number of tries: #{count}"
+      break
+    end
     break
   end
-
-  print 'Do you wish to continue? (Y/n) '
-  continue = gets.chomp
-  break unless continue.downcase.start_with?('y')
-
 end
-
-puts
-puts 'Mission aborted!'
