7e79ddbb9e41c6b558d1f74bcb3661720e0571e8
Jason Bornhoft <jmvbxx@gmail.com>
2021-02-16 20:08:17 -0500

    More testing of multiple classes

--- version_b/mission_control.rb
+++ version_b/mission_control.rb
@@ -1,7 +1,12 @@
 class MissionControl
 
-  def initialize
-    @mission_instance = Mission.new
+  def initialize(mission_instance: nil)
+    @mission_instance = mission_instance
+  end
+
+  def test
+    p @mission_instance.distance_traveled
+    p Mission::FUEL_CAPACITY
   end
 
   def launch_sequence
