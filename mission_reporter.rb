1e4a6b6ab3801de72993c4b929752510dfa887a1
Jason Bornhoft <jmvbxx@gmail.com>
2021-03-28 21:41:14 -0500

    Consolidate print methods using heredoc

--- mission_reporter.rb
+++ mission_reporter.rb
@@ -8,21 +8,27 @@ class MissionReporter
   end
 
   def print_status
-    puts 'Mission status:'
-    puts "  Current fuel burn rate: #{Mission::BURN_RATE_IN_L_PER_MIN} liters/min"
-    puts "  Current speed: #{(mission.current_speed * Mission::SECONDS_PER_HOURS).round(2)} km/h"
-    puts "  Elapsed time: #{seconds_to_hms(mission.elapsed_time)}"
-    puts "  Distance traveled: #{mission.distance_traveled.round(2)} km"
-    puts "  Time to destination: #{mission.time_to_destination.round(2)} seconds"
+    status = <<~STATUS
+      Mission status:
+        Current fuel burn rate: #{Mission::BURN_RATE_IN_L_PER_MIN} liters/min
+        Current speed: #{(mission.current_speed * Mission::SECONDS_PER_HOURS).round(2)} km/h
+        Elapsed time: #{seconds_to_hms(mission.elapsed_time)}
+        Distance traveled: #{mission.distance_traveled.round(2)} km
+        Time to destination: #{mission.time_to_destination.round(2)} seconds
+    STATUS
+    puts status
   end
 
   def print_summary
-    puts 'Mission summary:'
-    puts "  Total distance traveled: #{MissionControl.total_distance_traveled.round(2)} km"
-    puts "  Number of aborts and retries: #{Mission.aborts}/#{MissionControl.retries}"
-    puts "  Number of explosions: #{Mission.explosions}"
-    puts "  Total fuel burned: #{mission.total_fuel_burned.round(0)} liters"
-    puts "  Flight time: #{seconds_to_hms(MissionControl.total_elapsed_time)}"
+    summary = <<~SUMMARY
+      Mission summary:
+        Total distance traveled: #{MissionControl.total_distance_traveled.round(2)} km
+        Number of aborts and retries: #{Mission.aborts}/#{MissionControl.retries}
+        Number of explosions: #{Mission.explosions}
+        Total fuel burned: #{mission.total_fuel_burned.round(0)} liters
+        Flight time: #{seconds_to_hms(MissionControl.total_elapsed_time)}
+    SUMMARY
+    puts summary
   end
 
   private
