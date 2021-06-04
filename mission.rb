c5fa00f0f2f7072c2568cc4bad7e4749ac917ab8
Jason Bornhoft <jmvbxx@gmail.com>
2021-06-04 13:06:40 -0500

    Clean up relationship between mission and space_craft

--- mission.rb
+++ mission.rb
@@ -5,21 +5,18 @@ class Mission
 
   attr_reader :elapsed_time, :distance_traveled
 
-  attr_accessor :mission_reporter
-
   @aborts = 0
   @explosions = 0
   class << self
     attr_accessor :aborts, :explosions
   end
 
-  def initialize(name: nil, mission_reporter: MissionReporter.new(self, SpaceCraft.new(self)))
+  def initialize(name: nil, space_craft: SpaceCraft.new)
     @name = name
-    @mission_reporter = mission_reporter
     @elapsed_time = 0
     @distance_traveled = 0
     @aborted = false
-    @space_craft = SpaceCraft.new(self)
+    @space_craft = space_craft
   end
 
   def continue?
@@ -42,6 +39,11 @@ class Mission
     @aborted = true
   end
 
+  # TODO: This is not calculating properly over multiple missions
+  def total_fuel_burned
+    SpaceCraft::BURN_RATE_IN_L_PER_MIN * elapsed_time / SpaceCraft::SECONDS_PER_MINUTE
+  end
+
   private
 
   def name
@@ -59,7 +61,7 @@ class Mission
     # TODO: When mission aborts it doesn't 'reset' when another mission is run
     # and the entire `engage_afterburner` method is skipped on the next
     # `event_sequence` run
-    if one_in_number(3)
+    if one_in_number(99)
       puts 'Mission aborted!'
       self.class.aborts += 1
       name if rename?
@@ -85,20 +87,24 @@ class Mission
     return abort! unless continue? && prompt_user('Launch?')
 
     puts 'Launched!'
-    if one_in_number(5)
+    if one_in_number(99)
       distance_to_explosion = rand(SpaceCraft::TARGET_DISTANCE_IN_KMS)
-      launch_step while @space_craft.distance_traveled <= distance_to_explosion
+      launch_step while distance_traveled <= distance_to_explosion
       self.class.explosions += 1
       puts 'Your rocket exploded!'
     else
-      launch_step while @distance_traveled <= SpaceCraft::TARGET_DISTANCE_IN_KMS
+      launch_step while distance_traveled <= SpaceCraft::TARGET_DISTANCE_IN_KMS
     end
   end
 
   def launch_step
     @elapsed_time += 5
-    @distance_traveled = @space_craft.current_distance_traveled
+    @distance_traveled = current_distance_traveled
     # binding.pry
-    mission_reporter.print_status
+    # mission_reporter.print_status
+  end
+
+  def current_distance_traveled
+    @space_craft.current_speed * elapsed_time
   end
 end
