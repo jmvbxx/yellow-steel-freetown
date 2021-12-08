0739e196cee59beb839d0b65ae67259bd498be3f
Jason Bornhoft <jmvbxx@gmail.com>
2021-12-08 16:25:20 -0500

    Finish the completes successfully block

--- spec/mission_control_spec.rb
+++ spec/mission_control_spec.rb
@@ -6,13 +6,12 @@ require '../mission'
 require '../mission_control'
 require '../mission_reporter'
 require '../mission_plan'
-require 'pry'
 
 RSpec.describe MissionControl do
   let!(:mission) { Mission.new(name: 'Minerva') }
   let!(:mission_control) { MissionControl.new(mission: mission) }
 
-  context 'integration' do
+  describe 'integration' do
     it 'counts aborts across every prompt' do
       expect(mission).to receive(:one_in_number).and_return(true)
       expect($stdin).to receive(:gets).and_return('test-mission', 'y', 'y', 'n', 'n')
@@ -24,13 +23,20 @@ RSpec.describe MissionControl do
     end
 
     it 'completes successfully' do
-      allow(mission).to receive(:one_in_number).and_return(false)
-      allow(mission.space_craft).to receive(:current_speed).and_return(0.416667)
-      expect($stdin).to receive(:gets).and_return('test-mission', 'y', 'y', 'y', 'y', 'y', 'n')
+      allow_any_instance_of(Mission).to receive(:one_in_number).and_return(false)
+      allow_any_instance_of(SpaceCraft).to receive(:current_speed).and_return(0.416667)
+      expect($stdin).to receive(:gets).and_return('test-mission', 'y', 'y', 'y', 'y', 'y', 'y',
+                                                  'test-mission', 'y', 'y', 'y', 'y', 'y', 'y',
+                                                  'test-mission', 'y', 'y', 'y', 'y', 'y', 'y',
+                                                  'test-mission', 'y', 'y', 'y', 'y', 'n', 'y',
+                                                  'test-mission', 'y', 'y', 'y', 'n', 'y',
+                                                  'test-mission', 'y', 'y', 'n', 'y',
+                                                  'test-mission', 'y', 'n', 'y',
+                                                  'test-mission', 'n', 'n')
 
       mission_control.launch_sequence
       expect { mission_control.mission_report }.to output(
-"Mission summary:\n  Total distance traveled: 160.42 km\n  Number of aborts and retries: 0/0\n  Number of explosions: 0\n  Total fuel burned: 1079495 liters\n  Total flight time: 00:06:25\n"
+"Mission summary:\n  Total distance traveled: 481.25 km\n  Number of aborts and retries: 0/7\n  Number of explosions: 0\n  Total fuel burned: 3238485 liters\n  Total flight time: 00:19:15\n"
       ).to_stdout
     end
   end
